401b00f52124eea426ff16328f41dc2dd0cd0023
Merouane Atig <merwan@patem.net>
2017-03-19 23:06:33 +0100

    Dockerize application

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const winston = require('winston');
 
+const PORT = 3000;
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  winston.info('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  winston.info(`Example app listening on port ${PORT}!`);
 });
